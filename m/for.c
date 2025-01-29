bcc98196c1408342009788bfdb6c9fc8f72fe573
Alexiatr <himmatovdilshod06@gmail.com>
2025-01-29 11:37:40 +0500

    masalalar

--- m/for.c
+++ m/for.c
@@ -3,12 +3,5 @@
 int main() {
     int sum = 0, num, i;
 
-    for (int i = 0; i < 10; i++) {
-        printf("Sonni kiriting: ");
-        scanf("%d", &num);
-        sum += num; // Har bir sonni yig'indi bilab qo'shib boradi
-    }
-    printf("Yig'indi: %d\n ", sum);
-
     return 0;
  }
